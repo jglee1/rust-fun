64b64138af79783c4fac45461495df0d62ed28b6
jglee1 <jglee1@gmail.com>
2023-12-29 19:41:21 -0500

    added command line argument handling

---
+++ examples/16-error-handling/error-handling/src/main.rs
@@ -0,0 +1,38 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // The first argument is the path that was used to call the program.
+    println!("My path is {}.", args[0]);
+
+    println!("args length is {}", args.len());
+    for arg in args.iter() {
+        println!("{}", arg);
+    }
+
+    let file = File::open("non_existent_file.txt");
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                panic!("File not found: {}", error)
+            }
+            _ => {
+                panic!("Error opening file: {}", error)
+            }
+        },
+    };
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}

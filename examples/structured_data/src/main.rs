78b7fb9deaa99b466bfac58c27ba36c317a01a5c
jglee1 <jglee1@gmail.com>
2023-12-29 20:24:38 -0500

    updates

--- examples/structured_data/src/main.rs
+++ examples/structured_data/src/main.rs
@@ -3,6 +3,12 @@ struct Person {
     first_name: String,
     last_name: String,
     age: u8,
+    email: Option<String>,
+    phone: Option<String>,
+}
+
+fn get_full_name(person: &Person) -> String {
+    format!("{} {}", person.first_name, person.last_name)
 }
 
 fn main() {
@@ -10,6 +16,9 @@ fn main() {
         first_name: "John".to_string(),
         last_name: "Doe".to_string(),
         age: 25,
+        email: Some("abc@cde.com".to_string()),
+        phone: None,
     };
     println!("{:?}", person);
+    println!("Full name: {}", get_full_name(&person));
 }
